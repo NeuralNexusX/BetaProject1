a20bd02b72d63f331b33eb47bab48863c9c0ff1b
Alis Durna <>
2024-07-16 17:51:50 +0300

    beta0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">HELLO WORLD</div>;
+  return (
+    <div className="App">
+      <div>
+        <h1>Hello World 1</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
